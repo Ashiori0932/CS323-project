8225ed6756795609a2089c57cdf257b2a49a78bd
Ashiori0932 <1043901311@qq.com>
2024-11-16 21:14:25 +0800

    Tree display optimization

--- project1/antlr4/spltest.java
+++ project1/antlr4/spltest.java
@@ -7,14 +7,56 @@ import SPL.splParser;
 
 public class spltest {
     public static void main(String[] args) throws IOException {
-        InputStream is = new FileInputStream("./test/test02.spl"); // or System.in;
+        String inputFilePath = "./test/test02.spl"; 
+
+        String inputFileName = new File(inputFilePath).getName();
+        String outputFileName = inputFileName.substring(0, inputFileName.lastIndexOf('.')) + "_parse_tree.txt";
+
+        File outputDir = new File("./output");
+        if (!outputDir.exists()) {
+            boolean created = outputDir.mkdirs();
+        }
+
+        InputStream is = new FileInputStream(inputFilePath);
         ANTLRInputStream input = new ANTLRInputStream(is);
         splLexer lexer = new splLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         splParser parser = new splParser(tokens);
-        ParseTree tree = parser.program(); // program is the starting rule
+        ParseTree tree = parser.program();
 
-        System.out.println(tree.toStringTree(parser));
-        System.out.println();
+        String formattedTree = formatTree(tree, 0);
+
+        System.out.println(formattedTree);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("./output/" + outputFileName))) {
+            writer.write(formattedTree);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String formatTree(ParseTree tree, int level) {
+        StringBuilder sb = new StringBuilder();
+        String indent = "  ".repeat(level); 
+
+        sb.append(indent).append("("); 
+        sb.append(tree.getClass().getSimpleName());
+
+        if (tree instanceof TerminalNode) {
+            TerminalNode terminalNode = (TerminalNode) tree;
+            sb.append(" ").append(terminalNode.getText()); 
+        }
+
+        if (tree.getChildCount() > 0) {
+            sb.append("\n"); 
+            for (int i = 0; i < tree.getChildCount(); i++) {
+                sb.append(formatTree(tree.getChild(i), level + 1)); 
+            }
+            sb.append(indent).append(")").append("\n"); 
+        } else {
+            sb.append(")").append("\n"); 
+        }
+
+        return sb.toString();
     }
 }
